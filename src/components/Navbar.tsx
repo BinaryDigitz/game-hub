a1186a96d12dad7692159f0042aaeb3867feb3da
Binary Digitz <borisndoh@gmail.com>
2024-11-29 16:11:34 +0100

    Initital commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import logo from "../assets/logo.webp"
+
+function Navbar() {
+  return (
+    <nav className='container'>
+        <div className="log">{logo}</div>
+      
+    </nav>
+  )
+}
+
+export default Navbar
