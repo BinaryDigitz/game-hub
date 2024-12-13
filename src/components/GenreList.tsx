752a7d7899d59d7c3c7f2184fd6f22f53620f576
Binary Digitz <borisndoh@gmail.com>
2024-12-13 14:23:32 +0100

    Clean up genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -12,16 +12,19 @@ function GenreList({onSelectGenre}:Props) {
   if(error) return null;
   if (isLoading) return <p className={` h-screen text-center text-lg ${themeContext?.colorMode.sideColor}`}>Loading....</p>;
   return (
-    <ul>
+   <div className={themeContext?.colorMode.sideColor}>
+    <h2 className="font-bold text-xl p-2">Genres</h2>
+     <ul>
       {data.map((genre) => (
         <li key={genre.id}>
           <button onClick={() => onSelectGenre(genre)} className={`flex gap-2 p-2 hover:cursor-pointer hover:shadow-md active:translate-y-2 w-full ${themeContext?.colorMode.sideColor} hover:translate-x-1 trans`}>
             <img src={genre.image_background} className="size-8 rounded-sm " alt="" />
-            <p>{genre.name}</p>
+            <p className="text-no-wrap text-left">{genre.name}</p>
           </button>
         </li>
       ))}
     </ul>
+   </div>
   );
 }
 export default GenreList;
